d437bdd07fb041bf0847f9dfdec65c20909b1bbb
lihang <lihang@didachuxing.com>
2020-11-10 15:52:38 +0800

    PMOPP-599:权限配置

--- internal/app/portal/controllers/controllers.go
+++ internal/app/portal/controllers/controllers.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
-	"go.didapinche.com/foundation/apollo-plus/internal/pkg/constans"
 	"go.didapinche.com/foundation/apollo-plus/internal/pkg/http"
 	"go.didapinche.com/uic"
 )
@@ -21,29 +20,29 @@ func InitControllersFn(
 
 		{
 			r.GET("/app_namespace/:env", uic.AuthLogin(), appNamespaceController.FindAppNamespaceByAppIdAndClusterName)
-			r.POST("/app_namespace/:env", uic.AuthPerm(constans.AppOperatePerm), appNamespaceController.Create)
-			r.DELETE("/app_namespace/:env", uic.AuthPerm(constans.AppOperatePerm), appNamespaceController.DeleteById)
-			r.DELETE("/app_namespace_by_name/:env", uic.AuthPerm(constans.AppOperatePerm), appNamespaceController.DeleteByNameAndAppId)
-			r.PUT("/app_namespace/:env", uic.AuthPerm(constans.AppOperatePerm), appNamespaceController.Update)
-			r.POST("/app_namespace_by_related/:env", uic.AuthPerm(constans.AppOperatePerm), appNamespaceController.CreateByRelated)
+			r.POST("/app_namespace/:env", uic.AuthLogin(), appNamespaceController.Create)
+			r.DELETE("/app_namespace/:env", uic.AuthLogin(), appNamespaceController.DeleteById)
+			r.DELETE("/app_namespace_by_name/:env", uic.AuthLogin(), appNamespaceController.DeleteByNameAndAppId)
+			r.PUT("/app_namespace/:env", uic.AuthLogin(), appNamespaceController.Update)
+			r.POST("/app_namespace_by_related/:env", uic.AuthLogin(), appNamespaceController.CreateByRelated)
 			r.GET("/app_namespace_all/:env", uic.AuthLogin(), appNamespaceController.FindAppNamespaceByAppId)
 		}
 		{
 			r.GET("/items/:env", uic.AuthLogin(), itemController.FindItemByNamespaceId)
 			r.GET("/items_by_key/:env", uic.AuthLogin(), itemController.FindItemByKeyForPage)
 			r.GET("/items_by_key_on_app/:env", uic.AuthLogin(), itemController.FindAppItemByKeyForPage)
-			r.POST("/item/:env", uic.AuthPerm(constans.AppOperatePerm), itemController.Create)
-			r.POST("/item_by_text/:env", uic.AuthPerm(constans.AppOperatePerm), itemController.CreateByText)
-			r.DELETE("/item/:env", uic.AuthPerm(constans.AppOperatePerm), itemController.DeleteById)
-			r.PUT("/item/:env", uic.AuthPerm(constans.AppOperatePerm), itemController.Update)
+			r.POST("/item/:env", uic.AuthLogin(), itemController.Create)
+			r.POST("/item_by_text/:env", uic.AuthLogin(), itemController.CreateByText)
+			r.DELETE("/item/:env", uic.AuthLogin(), itemController.DeleteById)
+			r.PUT("/item/:env", uic.AuthLogin(), itemController.Update)
 			r.GET("/item_comment/:env", uic.AuthLogin(), itemController.FindAllComment)
-			r.DELETE("/items/:env", uic.AuthPerm(constans.AppOperatePerm), itemController.DeleteByNamespaceId)
+			r.DELETE("/items/:env", uic.AuthLogin(), itemController.DeleteByNamespaceId)
 			r.GET("/item/:env", uic.AuthLogin(), itemController.FindItemByNamespaceIdAndKey)
 			r.GET("/item_by_key_and_app_id/:env", uic.AuthLogin(), itemController.FindItemByAppIdAndKey)
 		}
 		{
-			r.POST("/release/:env", uic.AuthPerm(constans.AppOperatePerm), releaseController.Create)
-			r.POST("/release_gray_total/:env", uic.AuthPerm(constans.AppOperatePerm), releaseController.ReleaseGrayTotal)
+			r.POST("/release/:env", uic.AuthLogin(), releaseController.Create)
+			r.POST("/release_gray_total/:env", uic.AuthLogin(), releaseController.ReleaseGrayTotal)
 		}
 		{
 			r.GET("/release_history/:env", uic.AuthLogin(), historyController.Find)
